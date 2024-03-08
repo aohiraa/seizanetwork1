ba36a2120aea5c7b8c2d1c2701522be826c3f404
aohiraa <161918799+aohiraa@users.noreply.github.com>
2024-03-09 02:56:05 +0300

    config.ts

--- config.ts
+++ config.ts
@@ -7,7 +7,7 @@ export const window = {
     resizable: false,
     maximizable: false,
     fullscreenable: false,
-    title: 'Aurora Launcher',
+    title: 'SeizaNetwork',
 };
 
 export const api = {
